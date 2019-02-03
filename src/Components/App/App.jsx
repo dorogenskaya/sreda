8b3814d93c4ecf2f05be7dfba823d450b0fa25f3
Dovgalenok <drsasha5@gmail.com>
2019-02-03 16:05:46 +0300

    Added depedensies for routes and ant design. Added new components

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import Main from "../Main/Main";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {/*header*/}
+                <header className="header">
+                    <Link className="logo" to="/"><img src={logo} className="App-logo" alt="logo"/></Link>
+                    <nav className="header-nav">
+                        <ul className="nav-list">
+                            <li className="nav-list-item"><Link className="nav-list-item-link" to="/">Home</Link></li>
+                            <li className="nav-list-item"><Link className="nav-list-item-link" to="theme">Theme Page</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Main/>
+            </div>
+        );
+    }
+}
+
+export default App;
