4e32673b2d14117b3e2d4a78f1f5fbf878ed82d9
doraby <doraby1@gmail.com>
2019-02-16 14:09:52 +0300

    New functionality reset of the questions filter Refactor: Clean up source objects Add componentDidMount method Extracting states in the render method replace filter of answers in the render method from handleQuestions click specify some method names

--- src/util/paginate.js
+++ src/util/paginate.js
@@ -1,10 +1,7 @@
 import _ from "lodash";
 
 export function paginate(items, pageNumber, pageSize) {
-    console.log(items);
     const startIndex = (pageNumber - 1) * pageSize;
-    console.log(startIndex);
-
     return _(items)
         .slice(startIndex)
         .take(pageSize)
