78a340054d778d2adfca3c087b20fa24cd52b237
doraby <doraby1@gmail.com>
2019-04-06 10:40:47 +0300

    try to fix issue with default props

--- src/Components/Theme/createAnswer.jsx
+++ src/Components/Theme/createAnswer.jsx
@@ -1,93 +1,94 @@
 import React from "react";
 import {Drawer, Button} from 'antd';
-
-export default class CreateAnswer extends React.Component {
-
-    render() {
-        const { visible, onClose } = this.props;
-        console.log(visible, onClose);
-
-        const{ match: {params: { id }}, history, location} = this.props;
-        console.log( location);
-
-
-        return (
-            <Drawer
-                title={`Добавить ответ к теме ${id}`}
-                width={720}
-                onClose={() => {
-                    history.push(location.pathname.replace(/create-answer.*$/i, ''));
-                    return onClose;
-                }}
-                visible
-                style={{
-                    overflow: 'auto',
-                    height: 'calc(100% - 108px)',
-                    paddingBottom: '108px',
-                }}
-            >
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        bottom: 0,
-                        width: '100%',
-                        borderTop: '1px solid #e9e9e9',
-                        padding: '10px 16px',
-                        background: '#fff',
-                        textAlign: 'right',
-                    }}
-                >
-                    <Button onClick={onClose} style={{ marginRight: 8 }}>
-                        Отмена
-                    </Button>
-                    <Button onClick={onClose} type="primary">
-                        Отправить
-                    </Button>
-                </div>
-            </Drawer>
-        );
-    }
-}
-
 //
-// export default function CreateAnswer ({match: {params: { id }}, history, location}) {
-//     const { visible, onClose } = this.props;
+// export default class CreateAnswer extends React.Component {
 //
-//     return (
-//         <Drawer
-//             title={`Добавить ответ к теме ${id}`}
-//             onClose={() => {
-//                 history.push(location.pathname.replace(/create-answer.*$/i, ''));
-//             }}
-//             width={720}
-//             visible={visible}
-//             style={{
-//                 overflow: 'auto',
-//                 height: 'calc(100% - 108px)',
-//                 paddingBottom: '108px',
-//             }}
+//     render() {
+//         const { visible, onClose } = this.props;
+//         console.log(visible, onClose);
 //
-//         >
-//             <div
+//         const{ match: {params: { id }}, history, location} = this.props;
+//         console.log( location);
+//
+//
+//         return (
+//             <Drawer
+//                 title={`Добавить ответ к теме ${id}`}
+//                 width={720}
+//                 onClose={() => {
+//                     history.push(location.pathname.replace(/create-answer.*$/i, ''));
+//                     return onClose;
+//                 }}
+//                 visible
 //                 style={{
-//                     position: 'absolute',
-//                     left: 0,
-//                     bottom: 0,
-//                     width: '100%',
-//                     borderTop: '1px solid #e9e9e9',
-//                     padding: '10px 16px',
-//                     background: '#fff',
-//                     textAlign: 'right',
+//                     overflow: 'auto',
+//                     height: 'calc(100% - 108px)',
+//                     paddingBottom: '108px',
 //                 }}
 //             >
-//                 <Button onClick={onClose} style={{ marginRight: 8 }}>
-//                     Отмена
-//                 </Button>
-//                 <Button onClick={onClose} type="primary">
-//                     Отправить
-//                 </Button>
-//             </div>
-//         </Drawer>
-//     );
+//                 <div
+//                     style={{
+//                         position: 'absolute',
+//                         left: 0,
+//                         bottom: 0,
+//                         width: '100%',
+//                         borderTop: '1px solid #e9e9e9',
+//                         padding: '10px 16px',
+//                         background: '#fff',
+//                         textAlign: 'right',
+//                     }}
+//                 >
+//                     <Button onClick={onClose} style={{ marginRight: 8 }}>
+//                         Отмена
+//                     </Button>
+//                     <Button onClick={onClose} type="primary">
+//                         Отправить
+//                     </Button>
+//                 </div>
+//             </Drawer>
+//         );
+//     }
 // }
+
+
+export default function CreateAnswer ({match: {params: { id }}, history, location}) {
+    const { visible, onClose } = this.props;
+    console.log(id);
+
+    return (
+        <Drawer
+            title={`Добавить ответ к теме ${id}`}
+            onClose={() => {
+                history.push(location.pathname.replace(/create-answer.*$/i, ''));
+            }}
+            width={720}
+            visible
+            style={{
+                overflow: 'auto',
+                height: 'calc(100% - 108px)',
+                paddingBottom: '108px',
+            }}
+
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    bottom: 0,
+                    width: '100%',
+                    borderTop: '1px solid #e9e9e9',
+                    padding: '10px 16px',
+                    background: '#fff',
+                    textAlign: 'right',
+                }}
+            >
+                <Button onClick={onClose} style={{ marginRight: 8 }}>
+                    Отмена
+                </Button>
+                <Button onClick={onClose} type="primary">
+                    Отправить
+                </Button>
+            </div>
+        </Drawer>
+    );
+}
