d306940f2a74e1bc57f16acbaa96a4a3cfbb9c08
doraby <doraby1@gmail.com>
2019-12-09 23:35:42 +0300

    change collection logic in did mount

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -6,56 +6,96 @@ class Rating extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userLists: []
+            userLists: [],
+            userListsAll:[]
         };
     }
+
     componentDidMount () {
-        // take userSubjectsKeys from coincounts new
-        // left max 4 with the biggest number
-        const userSubjectsKeys = [
-            '-LfViy9MwyKAAk1QCyJO',
-            '-Lb9fI5xXlQWJfDilNru',
-            '-LeGgXVgvvOlaonvEQCc'
-        ];
+        let object = this.props.user.coinCounter;
+        let arrayNew = [], arrayAll =[];
+        for(let key in object){
+            if(object[key].new){
+                arrayNew.push({key: key, subjectName:object[key].subjectName});
+            } else {
+                arrayAll.push({key: key, subjectName:object[key].subjectName});
+            }
+        }
+        arrayNew.sort((a,b) => b.new - a.new);
+        arrayAll.sort((a,b) => b.new - a.new);
+        let userSubjectsKeys = arrayNew.concat(arrayAll);
+        userSubjectsKeys.splice(3);
         const userLists = [];
+        const userListsAll = [];
+
         database
             .ref('users')
             .once("value", snapshot => {
                 let users = snapshot.val();
-                for (let subject of userSubjectsKeys){
-                    console.log(subject, 'subject');
-                    for (let key in users){
-                        let user = users[key];
-                        /*** @param {{coinCounter:string}} user*/
-                        if(!user.coinCounter || !user.coinCounter[subject]){
+                for (let key in users){
+                    let user = users[key];
+                    const userList = [];
 
-                        } else {
-                            const userList = [];
-                            userList.push({
-                                name: user.name,
-                                picture: user.picture,
-                                uid: user.picture,
-                                role: user.role,
-                                coins: user.coinCounter[subject].new,
-                                allCoins: user.coinCounter[subject].all
-                            });
-                            userLists.push({subject:subject, userList:userList });
-                        }
-                    };
+                    // add allCoins
+                    userListsAll.push({
+                        name: user.name,
+                        picture: user.picture,
+                        uid: user.uid,
+                        role: user.role,
+                        allCoins: user.coinCounter
+                    });
 
-                }
-            })
-            .then(() => {this.setState({userLists})});
+                    for (let keySubject in user.coinCounter ){
+                        const subject = userSubjectsKeys.find(({key}) => key === `${keySubject}`);
+
+                        if (userSubjectsKeys.filter(element => {return element.key === keySubject})) {
+                            if (userLists.length === 0) {
+                                userList.push({
+                                    name: user.name,
+                                    picture: user.picture,
+                                    uid: user.uid,
+                                    role: user.role,
+                                    coins: user.coinCounter[keySubject].new,
+                                    allCoins: user.coinCounter[keySubject].all
+                                });
+                                userLists.push({subject:{subject:keySubject, subjectName:subject.subjectName }, userList:userList });
+                            } else {
+                                userLists.forEach(element => {
+                                    if (element.subject.subject === keySubject) {
+                                        element.userList.push({
+                                            name: user.name,
+                                            picture: user.picture,
+                                            uid: user.uid,
+                                            role: user.role,
+                                            coins: user.coinCounter[keySubject].new,
+                                            allCoins: user.coinCounter[keySubject].all
+                                        });
+                                    } else {
+                                        userList.push({
+                                            name: user.name,
+                                            picture: user.picture,
+                                            uid: user.uid,
+                                            role: user.role,
+                                            coins: user.coinCounter[keySubject].new,
+                                            allCoins: user.coinCounter[keySubject].all
+                                        });
+                                        userLists.push({subject:{subject:keySubject, subjectName: subject.subjectName}, userList:userList });
+                                    }
+                            })
+                    ;}
+                }}
+            }})
+            .then(() => {this.setState({userLists, userListsAll })});
     }
 
     render() {
-        const {userLists} = this.state;
+        const {userLists, userListsAll} = this.state;
         return (
             <div className="Theme">
                 <div className="Theme-content">
-                    {userLists.map(object =>
+                    {userLists.map((object, index )=>
                          <RatingWidget
-                            key={object.subject}
+                            key={index}
                             user={this.props.user}
                             subject={object.subject}
                             userList={object.userList}/>
@@ -65,7 +105,11 @@ class Rating extends Component {
 
                 <div className="Theme-questions">
                     <h1>Общий рейтинг</h1>
-                    <RatingWidget/>
+                    <RatingWidget
+                        user={this.props.user}
+                        userList={userListsAll}
+                        subject={{subject:'0', subjectName:'All' }}
+                    />
                 </div>
             </div>
         );
--- src/Components/Rating/RatingWidget.jsx
+++ src/Components/Rating/RatingWidget.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import RatingTable from "./RatingTable";
 import Tab from "../common/Tab";
 import Pagination from "../common/pagination";
+import { Spin } from 'antd';
 
 import  _ from 'lodash';
 import {paginate} from "../../util/paginate";
@@ -14,21 +15,15 @@ class RatingWidget extends Component {
             userList: this.props || [],
             currentPage: 1,
             pageSize: 15,
-            subject: this.props.subject
+            subject: 'Loading subject rating',
+            loading: true,
         };
     }
     componentDidMount () {
-        // const {userList} = this.props || [];
-        // this.setState({userList});
+        if (!this.props.subject)
+            return;
+            return this.setState({subject: this.props.subject, loading: false});
     }
-    //
-    // getUsers = (activeTab) => {
-    // };
-    //
-    // setDataUsers = (userList) => {
-    //     this.setState({userList})
-    // };
-
 
     handleTab = (activeTab) => {
         console.log(activeTab);
@@ -40,39 +35,46 @@ class RatingWidget extends Component {
     };
 
     render() {
-        const {userList, activeTab,  currentPage, pageSize} = this.state;
+        const {loading, userList, activeTab,  currentPage, pageSize, subject} = this.state;
 
         const sorted =
             _.orderBy(userList, ['coins'], ['desc'])
             .map((user, index) => {return {picture: user.picture, name: user.name, coins: user.coins, key: index+1};
         });
-        console.log('sorted');
         const paginated = paginate(sorted, currentPage, pageSize);
-        console.log('paginated');
 
-        return (
-            <React.Fragment>
-                <h1>subjectName</h1>
-                <Tab
-                    items = {['неделя', 'всё время']}
-                    handleClick={this.handleTab}
-                    activeTab={activeTab}
-                    classCss={'tab_h2'}
-                />
-                <RatingTable
-                    userList={paginated}
-                />
+        if(loading)
+            return (
+                <div style={{textAlign: "center", position: "absolute", top: "25%", left: "50%"}}>
+                    <h2>Loading...</h2>
+                    <Spin size="large"/>
+                </div>
+            )
 
-                <Pagination
-                    onChange={this.handlePageClick}
-                    total={userList ? userList.length :0}
-                    pageSize={pageSize}
-                    current={currentPage}
-                    hideOnSinglePage={true}
-                />
+            return (
+                <React.Fragment>
+                    <h1>{subject.subjectName}</h1>
+                    <Tab
+                        items = {['неделя', 'всё время']}
+                        handleClick={this.handleTab}
+                        activeTab={activeTab}
+                        classCss={'tab_h2'}
+                    />
+                    <RatingTable
+                        userList={paginated}
+                    />
 
-            </React.Fragment>
-        );
+                    <Pagination
+                        onChange={this.handlePageClick}
+                        total={userList ? userList.length :0}
+                        pageSize={pageSize}
+                        current={currentPage}
+                        hideOnSinglePage={true}
+                    />
+
+                </React.Fragment>
+            );
+
     }
 }
 
